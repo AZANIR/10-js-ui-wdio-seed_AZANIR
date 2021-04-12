628707f5d5598399ade6fa714d558cdd5391adb9
AZANIR <eleoneks@gmail.com>
2021-04-12 11:40:32 +0300

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+require("ts-node").register({ files: true });
+
 exports.config = {
     //
     // ====================
@@ -24,7 +26,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -97,7 +99,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: '',
+    baseUrl: 'https://funnyducks.com.ua',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
